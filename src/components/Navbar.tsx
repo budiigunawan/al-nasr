12a2f5ea7c0bba5824ca0d3e1696eddd3762c109
Budi Gunawan <budiindra.g@gmail.com>
2023-12-08 14:51:31 +0700

    add style to navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,86 @@
-import { Link } from 'react-router-dom';
+import { useMemo, useCallback } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import Logo from '@/assets/img/logo.svg';
 import { Button } from './ui/Button';
 
 export const Navbar = () => {
+  const location = useLocation();
+  const isHome: boolean = useMemo(() => {
+    return location.hash === '';
+  }, [location.hash]);
+
+  const isActive = useCallback(
+    (hash: string) => {
+      return location.hash === hash;
+    },
+    [location.hash]
+  );
+
   return (
     <>
-      <header>
-        <h1>Logo</h1>
+      <header className='container navbar'>
+        <img src={Logo} height={'68px'} alt='al nasr logo' />
         <nav>
-          <ul>
+          <ul className='navbar__menu'>
             <li>
-              <Link to='/'>Home</Link>
+              <Link
+                to='/home'
+                className={
+                  isHome
+                    ? 'navbar__menu__link navbar__menu__link--active'
+                    : 'navbar__menu__link'
+                }
+              >
+                Logo
+              </Link>
             </li>
             <li>
-              <Link to='#about-us'>About Us</Link>
+              <Link
+                to='/home#about-us'
+                className={
+                  isActive('#about-us')
+                    ? 'navbar__menu__link navbar__menu__link--active'
+                    : 'navbar__menu__link'
+                }
+              >
+                About Us
+              </Link>
             </li>
             <li>
-              <Link to='#package'>Package</Link>
+              <Link
+                to='/home#packages'
+                className={
+                  isActive('#packages')
+                    ? 'navbar__menu__link navbar__menu__link--active'
+                    : 'navbar__menu__link'
+                }
+              >
+                Packages
+              </Link>
             </li>
             <li>
-              <Link to='#facilities'>Facilities</Link>
+              <Link
+                to='/home#facilities'
+                className={
+                  isActive('#facilities')
+                    ? 'navbar__menu__link navbar__menu__link--active'
+                    : 'navbar__menu__link'
+                }
+              >
+                Facilities
+              </Link>
             </li>
             <li>
-              <Link to='#gallery'>Gallery</Link>
+              <Link
+                to='/home#gallery'
+                className={
+                  isActive('#gallery')
+                    ? 'navbar__menu__link navbar__menu__link--active'
+                    : 'navbar__menu__link'
+                }
+              >
+                Gallery
+              </Link>
             </li>
           </ul>
         </nav>
