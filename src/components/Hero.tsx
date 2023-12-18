e7e68c02bafbb83b468bd81503e8176f18151a95
Budi Gunawan <budiindra.g@gmail.com>
2023-12-18 08:44:13 +0700

    add hero info and hero illustration

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
 import { Badge } from './ui/Badge';
+import { Button } from './ui/Button';
 
 export const Hero = () => {
   return (
-    <div className='container'>
-      <Badge text='No.1 in Indonesia' />
-    </div>
+    <section className='container'>
+      <div className='hero'>
+        <div className='hero__info'>
+          <Badge text='No.1 in Indonesia' />
+          <h1 className='hero__info__title'>
+            Make it easy for your umrah worship with Al Nasr Travel
+          </h1>
+          <p className='hero__info__subtitle'>
+            Facilitate your Umrah pilgrimage with us, we already have hundreds
+            of thousands of customers, you can go for Umrah to Mecca and Medina
+          </p>
+          <Button>Contact us</Button>
+        </div>
+        <div className='hero__illustration'>
+          <div className='hero__illustration__box'></div>
+          <div className='hero__illustration__video'>
+            <iframe
+              width={'406px'}
+              height={'507px'}
+              src='https://www.youtube.com/embed/rEDv_hUCB50'
+              title='Masjid Al Haram'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
