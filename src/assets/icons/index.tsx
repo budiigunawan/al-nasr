978401e9de812d20c1075022898bc45fefc80896
Budi Gunawan <budiindra.g@gmail.com>
2023-12-19 10:00:21 +0700

    add about component on desktop

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,6 @@
+import { FlightIcon } from './FlightIcon';
+import { TicketIcon } from './TicketIcon';
+import { HotelIcon } from './HotelIcon';
+import { VisaIcon } from './VisaIcon';
+
+export { FlightIcon, TicketIcon, HotelIcon, VisaIcon };
