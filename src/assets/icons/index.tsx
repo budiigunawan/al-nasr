30d6da5fbe2ae1391ed3636307b48c5adc120028
Budi Gunawan <budiindra.g@gmail.com>
2023-12-27 09:23:58 +0700

    create facilities component

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -4,5 +4,26 @@ import { HotelIcon } from './HotelIcon';
 import { VisaIcon } from './VisaIcon';
 import { MeatIcon } from './MeatIcon';
 import { StarIcon } from './StarIcon';
+import { SecurityIcon } from './SecurityIcon';
+import { ConselingIcon } from './ConselingIcon';
+import { VehicleIcon } from './VehicleIcon';
+import { HealthIcon } from './HealthIcon';
+import { HostelIcon } from './HostelIcon';
+import { MemberIcon } from './MemberIcon';
+import { VaccineIcon } from './VaccineIcon';
 
-export { FlightIcon, TicketIcon, HotelIcon, VisaIcon, MeatIcon, StarIcon };
+export {
+  FlightIcon,
+  TicketIcon,
+  HotelIcon,
+  VisaIcon,
+  MeatIcon,
+  StarIcon,
+  SecurityIcon,
+  ConselingIcon,
+  VehicleIcon,
+  HealthIcon,
+  HostelIcon,
+  MemberIcon,
+  VaccineIcon,
+};
